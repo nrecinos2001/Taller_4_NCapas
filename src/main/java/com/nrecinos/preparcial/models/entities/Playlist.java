b7313c11c9bb077c246c6ea3aaf36cd3ea3cb7ab
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-24 19:42:04 -0600

    :card_file_box: Add entity for Playlist

---
+++ src/main/java/com/nrecinos/preparcial/models/entities/Playlist.java
@@ -0,0 +1,43 @@
+package com.nrecinos.preparcial.models.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "playlist")
+public class Playlist {
+	@Id
+	@Column(name = "code")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID code;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "description")
+	private String description;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_code", nullable = false)
+	private User user;
+
+	public Playlist(String title, String description, User user) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.user = user;
+	}
+}
--- src/main/java/com/nrecinos/preparcial/models/entities/User.java
+++ src/main/java/com/nrecinos/preparcial/models/entities/User.java
@@ -1,10 +1,17 @@
 package com.nrecinos.preparcial.models.entities;
 
+import java.util.List;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +24,7 @@ import lombok.NoArgsConstructor;
 public class User {
 	@Id
 	@Column(name = "code")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID code;
 
 	@Column(name = "username")
@@ -27,4 +35,14 @@ public class User {
 
 	@Column(name = "password")
 	private String password;
+	
+	@OneToMany(mappedBy = "playlist", fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<Playlist> playlists;
+	
+	public User(String username, String email,String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
 }
