031bfd433836605e151f26765af3f0948b0223ce
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-24 19:03:17 -0600

    :tada: Initalize project

---
+++ src/main/java/com/nrecinos/library/PreParcialApplication.java
@@ -0,0 +1,13 @@
+package com.nrecinos.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreParcialApplication.class, args);
+	}
+
+}
