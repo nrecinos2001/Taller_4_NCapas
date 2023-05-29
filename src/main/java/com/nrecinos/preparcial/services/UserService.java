a7dcf0ae077a037c5f150656c56761e14e0185c2
Josuepp <62664664+Josuepp@users.noreply.github.com>
2023-05-28 23:37:46 -0600

    Add PlaylistController

--- src/main/java/com/nrecinos/preparcial/services/UserService.java
+++ src/main/java/com/nrecinos/preparcial/services/UserService.java
@@ -6,9 +6,9 @@ import com.nrecinos.preparcial.models.dtos.RegisterDTO;
 import com.nrecinos.preparcial.models.dtos.UpdatePasswordDTO;
 import com.nrecinos.preparcial.models.entities.User;
 
-public interface UserServices {
+public interface UserService {
 	void register(RegisterDTO info);
-	void save() throws Exception;
+	void saveUser() throws Exception;
 	void deleteById(UUID code) throws Exception;
 	User findOneById(UUID code);
 	void updatePassword(UpdatePasswordDTO password);
