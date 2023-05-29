ff454595666405c3f534d9eb8b1475a44fb16f9a
AlejandroOrteag17 <00166120@uca.edu.sv>
2023-05-28 22:09:57 -0600

    Adding Dtos fields and validations🎨

--- src/main/java/com/nrecinos/preparcial/models/dtos/UpdateSongTitleDTO.java
+++ src/main/java/com/nrecinos/preparcial/models/dtos/UpdateSongTitleDTO.java
@@ -1,5 +1,17 @@
 package com.nrecinos.preparcial.models.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UpdateSongTitleDTO {
 
+	@NotEmpty(message= "El identificador no puede estar vacio")
+	private String code;
+	
+	@NotEmpty(message= "El titulo no puede estar vacio")
+	private String newTitle;	
+	
 }
