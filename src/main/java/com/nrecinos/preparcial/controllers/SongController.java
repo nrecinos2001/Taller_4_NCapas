24f503cde628f7da9ade2b1fadab8738e2e1ce83
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-29 18:47:57 -0600

    :sparkles: Add songs controller

--- src/main/java/com/nrecinos/preparcial/controllers/SongController.java
+++ src/main/java/com/nrecinos/preparcial/controllers/SongController.java
@@ -1,5 +1,24 @@
 package com.nrecinos.preparcial.controllers;
 
-public class SongController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nrecinos.preparcial.models.entities.Song;
+import com.nrecinos.preparcial.services.SongService;
+
+@Controller
+@RequestMapping(value = "/songs")
+public class SongController {
+	@Autowired
+	private SongService songService;
+	
+	@GetMapping("/")
+	List<Song> getAll() {
+		List<Song> songs = songService.findAll();
+		return songs;
+	}
 }
