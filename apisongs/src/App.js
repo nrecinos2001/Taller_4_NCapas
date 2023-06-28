56f18f20260e39b1737fc1b62ebd7cc80fbd5fad
CristianH00216920 <00216920@uca.edu.sv>
2023-06-28 16:58:59 -0600

    validating login form

--- apisongs/src/App.js
+++ apisongs/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Songs from './pages/Songs';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-      <h5>API SONGS</h5>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/songs" element={<Songs />} /> 
+      </Routes>
     </>
   );
 }
--- apisongs/src/index.js
+++ apisongs/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './pages/Login'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
