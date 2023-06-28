367ad19367cb0ee05a3df59e283c29d5c21ab1a0
AlejandroOrteag17 <00166120@uca.edu.sv>
2023-06-27 22:17:56 -0600

    Register form complete and working ✨

--- apisongs/src/index.js
+++ apisongs/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from './pages/Login'
+import SingUp from './pages/SingUp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <SingUp />
   </React.StrictMode>
 );
 
