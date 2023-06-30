1ef132ddfbd237257fccb8a1643dfc741b321c36
CristianH00216920 <00216920@uca.edu.sv>
2023-06-29 19:24:03 -0600

    :bug: fix song filtering

--- apisongs/src/pages/Songs.js
+++ apisongs/src/pages/Songs.js
@@ -1,47 +1,90 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom';
 
-const SongList = () => {
+const Songs = () => {
     const [songs, setSongs] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredSongs, setFilteredSongs] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const token = localStorage.getItem('token');
-                console.log(token);
-                const response = await axios.get('http://localhost:8080/songs', {
+                let url = `http://localhost:8080/songs?page=${currentPage}`;
+                const response = await axios.get(url, {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
                 });
-                setSongs(response.data);
+                setSongs(response.data?.content);
+                setTotalPages(response.data?.totalPages);
             } catch (error) {
                 console.error(error);
             }
         };
 
         fetchData();
-    }, []);
+    }, [currentPage]);
+
+    useEffect(() => {
+        const filteredContent = songs.filter((song) =>
+            song.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredSongs(filteredContent);
+    }, [songs, searchTerm]);
+
+    const goToNextPage = () => {
+        if (currentPage < totalPages - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const goToPreviousPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const isLastPage = currentPage === totalPages - 1;
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
     return (
         <>
             <div className="container mt-2 ml-10">
-                <Link to="/dashboard"><button className="btn btn-info">REGRESAR</button></Link>
+                <Link to="/dashboard">
+                    <button className="btn btn-info">REGRESAR</button>
+                </Link>
             </div>
 
             <div className="container mt-2">
                 <div className="row justify-content-center">
                     <div className="col-6">
+                        <input
+                            type="text"
+                            className="form-control mb-2"
+                            placeholder="Buscar por título"
+                            value={searchTerm}
+                            onChange={handleSearch}
+                        />
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-6">
                         <table className="table table-dark text-center">
-                            <thead >
+                            <thead>
                                 <tr>
                                     <th>Título</th>
                                     <th>Duración</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {songs.map(song => (
+                                {filteredSongs.map((song) => (
                                     <tr key={song.code}>
                                         <td>{song.title}</td>
                                         <td>{song.duration}</td>
@@ -51,12 +94,43 @@ const SongList = () => {
                         </table>
                     </div>
                 </div>
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <button
+                            className="btn btn-primary"
+                            disabled={currentPage === 0}
+                            onClick={goToPreviousPage}
+                        >
+                            Anterior
+                        </button>
+                        <button
+                            className="btn btn-primary ml-2"
+                            disabled={isLastPage}
+                            onClick={goToNextPage}
+                        >
+                            Siguiente
+                        </button>
+                    </div>
+                </div>
             </div>
             <Outlet />
         </>
-
-
     );
 };
 
-export default SongList;
+export default Songs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
